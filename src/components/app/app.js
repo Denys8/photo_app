0f958a23346ed6722f65376651b94c276e74a4ea
Denys <yevtushenko.denis@gmail.com>
2019-02-14 22:54:55 +0200

    create structure of app

---
+++ src/components/app/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import AppHeader from "../appheader";
+
+import "./app.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Denys and Lily!!!</h1>
+        <AppHeader />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/appheader/appheader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./appheader.scss";
+
+const AppHeader = () => {
+  return (
+    <ul>
+      <li>Photos</li>
+      <li>Favorite Pfotos</li>
+    </ul>
+  );
+};
+
+export default AppHeader;
